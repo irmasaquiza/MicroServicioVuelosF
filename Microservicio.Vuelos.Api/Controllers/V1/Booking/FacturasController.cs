f8f758ad67627d2721a074902f2982ff93014562
irmasaquiza <nachitomv2005@hotmail.com>
2026-04-28 20:00:22 -0500

    antes de la gestion booking

---
+++ Microservicio.Vuelos.Api/Controllers/V1/Booking/FacturasController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservicio.Vuelos.Api.Controllers.V1.Booking
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FacturasController : ControllerBase
+    {
+    }
+}
---
+++ Microservicio.Vuelos.Business/DTOs/Booking/Cliente/ClienteBookingResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microservicio.Vuelos.Business.DTOs.Booking.Cliente
+{
+    internal class ClienteBookingResponse
+    {
+    }
+}
